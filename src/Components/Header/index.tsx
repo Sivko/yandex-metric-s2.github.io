d49cafa5f38619498b02e71ce46d2f60545aca91
Vlad Sivakov <svv@salesap.ru>
2024-05-11 02:06:42 +0300

    upd

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,11 +3,12 @@ import { useContext } from "react"
 
 export default function Header() {
 
-  const { metricId, setMetricId } = useContext(Context);
+  const { metricId, setMetricId, yandexToken } = useContext(Context);
 
   return (
     <div>
-      <div id="buttonYandexAuth" />
+
+      <div id="buttonYandexAuth" style={{ display: yandexToken ? "none" : "flex" }} />
       <div className=" flex justify-between items-center py-4">
         <input type="text" placeholder="ID метрики" value={metricId} onChange={e => setMetricId(e.target.value)} className="outline-none border-b" />
         <div>
